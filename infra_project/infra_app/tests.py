017846f2ee6dae1f89a83afe8db3ed6a488c1c41
Сергей <7atom7@mail.ru>
2021-07-22 16:30:34 +0300

    PEP8, Tests Final

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -19,5 +19,7 @@ class StaticPagesURLTests(TestCase):
         """Проверка контекста страниц."""
         response = self.guest_client.get('/')
         self.assertContains(response, 'У меня получилось!')
+
+
         response = self.guest_client.get('/second_page/')
-        self.assertContains(response, 'А это вторая страница!')
+        self.assertContains(response, 'А это вторая страница') 
